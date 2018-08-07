71f56894972753634a9d376383e832c134268eeb
Ratnam yadav <ratnam.yadav@icloud.com>
2018-08-07 23:25:08 +0530

    user association fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :follows
+  has_many :follows, foreign_key: :target_id
   has_many :deals
 
   def followed(user = nil)
