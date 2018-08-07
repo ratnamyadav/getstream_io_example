b8cf245269175db99d3609403d77b74a7245a7a6
Ratnam yadav <ratnam.yadav@icloud.com>
2018-08-07 23:16:15 +0530

    database.yml

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,10 @@ if Deal.count == 0
   user = User.last
   user.deals.create(name: 'Test Deal 1', description: 'Test deal 1 description.')
   user.deals.create(name: 'Test Deal 2', description: 'Test deal 2 description.')
+  user.deals.create(name: 'Test Deal 3', description: 'Test deal 3 description.')
+  user = User.first
+  user.deals.create(name: 'Test Deal 4', description: 'Test deal 4 description.')
+  user.deals.create(name: 'Test Deal 5', description: 'Test deal 5 description.')
+  user.deals.create(name: 'Test Deal 6', description: 'Test deal 6 description.')
+  user.deals.create(name: 'Test Deal 7', description: 'Test deal 7 description.')
 end
